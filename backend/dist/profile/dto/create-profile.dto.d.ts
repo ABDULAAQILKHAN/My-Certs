54b160b723247d5364473e1836b52410ba7fd1e6
Aaqil khan <aaqilpro99@gmail.com>
2025-09-23 20:12:54 +0530

    synced

---
+++ backend/dist/profile/dto/create-profile.dto.d.ts
@@ -0,0 +1,7 @@
+export declare class CreateProfileDto {
+    sub: string;
+    name: string;
+    email: string;
+    phone: string;
+    avatar?: string;
+}
---
+++ backend/dist/profile/entities/profile.entity.d.ts
@@ -0,0 +1,10 @@
+export declare class Profile {
+    id: string;
+    userId: string;
+    name: string;
+    email: string;
+    phone: string;
+    avatar: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
